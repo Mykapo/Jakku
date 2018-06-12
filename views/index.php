9a0700e6b53ddf9401319707c99d717cf415f43e
mona <mona.calibani@hetic.net>
2018-06-12 14:43:01 +0200

    css - fonts & icons import

--- views/index.php
+++ views/index.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Accueil</title>
+    <link rel="stylesheet" type="text/css" href="./styles/main.css">
 </head>
 <body>
-    
+    <?php require('partials/aside.php')?>
 </body>
 </html>
