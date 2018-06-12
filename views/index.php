05890b6ccfb37f25f7885ba610426c69658cd0bb
Clément Maurice <clement.maurice@hetic.net>
2018-06-12 15:28:42 +0200

    update setup

--- views/index.php
+++ views/index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/styles/main.css">
     <title>Accueil</title>
-    <link rel="stylesheet" type="text/css" href="./styles/main.css">
 </head>
 <body>
-    <?php require('partials/aside.php')?>
+    <?php require('partials/header.php'); ?>
 </body>
 </html>
---
+++ views/partials/header.php
@@ -0,0 +1,9 @@
+<header>
+  <div class="title">
+    <img src="assets/logo.svg" alt="">
+  </div>
+  <div class="search">
+    <img src="assets/search.svg" alt="" class="search-icon">
+    <input type="text" placeholder="Rechercher">
+  </div>
+</header>
