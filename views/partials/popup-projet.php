99c1c2d8db9f8f954d6c34a1fa1a0d89734ce0c8
Clément Maurice <clement.maurice@hetic.net>
2018-06-13 16:19:35 +0200

    added inte-popup-projet + title style

---
+++ views/partials/popup-projet.php
@@ -0,0 +1,27 @@
+<div class="popup">
+    <section class="popup-projet">
+        <h1>Ajouter un projet</h1>
+        <form action="" method="post">
+            <div>
+                <label for="name">Nom du projet</label>
+                <input type="text" name="" placeholder="Saisir">
+            </div>
+            <div>
+                <label for="client">Client</label>
+                <input type="text" name="client" placeholder="Saisir">
+            </div>
+            <div>
+                <label for="deadline">DeadLine</label>
+                <input type="text" name="deadline" placeholder="Saisir">
+            </div>
+            <div>
+                <label for="statut">Statut</label>
+                <select name="statut" id="">
+                    <option value="valeur1" selected>Valeur 1</option>
+                    <option value="valeur2">Valeur 2</option>
+                </select>
+            </div>
+            <button type="submit" class="add complete">Ajouter</button>
+        </form>
+    </section>
+</div>
